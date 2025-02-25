f3c0c858f4911c8ee0a63039ddf156d64b4eb229
Natink1 <natink4825@gmail.com>
2025-02-25 10:17:36 +0300

    Functionality

--- src/Petty.jsx
+++ src/Petty.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { useState } from "react";
 
 const Petty = () => {
-  return (
-    <div>Petty Cash</div>
-  )
-}
+  const [amount, setAmount] = useState(1000);
+  const [cashout, setCashout] = useState(0);
 
-export default Petty
+  const depositamount = () => {
+    const min = amount - cashout;
+    setAmount(min);
+  };
+
+  const handleCost = (e) => {
+    setCashout(e.target.value);
+  } 
+  
+
+  return(
+    <>
+      <div className="pt-5 flex justify-center">
+        <div className="flex flex-col items-center text-center">
+          <h1 className="text-4xl font-bold flex justify-center">Petty Cash</h1>
+          <p className=" pt-7 text-3xl">You Have ${amount}</p>
+          <input onChange={handleCost} type="number" className="mt-5"></input>
+          <button
+            
+            className="mt-12 font-bold bg-[#ffdd00] w-25 h-12 rounded-2xl hover:bg-[#ffb300] transition ease-linear"
+            onClick={depositamount}
+          >
+            
+            Cash Out
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Petty;
